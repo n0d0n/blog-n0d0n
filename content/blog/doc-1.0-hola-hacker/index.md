4a84b3353f0eebc4a85aea214f401915fc6b0790
n0d0n <contact@n0d0n.xyz>
2024-06-29 10:35:48 +0200

    First commit

---
+++ content/blog/doc-1.0-hola-hacker/index.md
@@ -0,0 +1,53 @@
+---
+title: doc-1.0-hello-hacker.txt
+date: "2024-06-28T15:12:44.015Z"
+description: "Hola hacker"
+---
+
+## Me presento
+
+Mi nombre es Bruno, aunque también puedes referirte a mi como
+<span style="font-family: monospace">n0d0n</span>. 
+
+Hace un par de años
+decidí aprender de forma autónoma sobre pentesting. Allá por 2022, después
+de varias aproximaciones de forma intermitente a 
+[HackTheBox](https://www.hackthebox.com/), tomo la decisión de realizar
+un máster en seguridad de la información. Siendo honestos, a día de hoy 
+no le recomendaría a mi yo pasado tomar este camino. El máster está bien
+si no tienes claro que rama del mundo ciber es la que encaja contigo. Para
+alguien que quiere enfocarse en el pentesting y la rama más técnica de la ciber,
+existen otros caminos. 
+
+Y en esos caminos de los que hablo es donde me encuentro actualmente. Con el
+máster ya finalizado, decido enfocarme en las certificaciones, en plataformas 
+CTF como [HackTheBox](https://www.hackthebox.com/), en leer contenido sobre
+hacking e "intentar" hacer un poco de Bug Bounty.
+
+## Este blog
+
+Hoy, 28 de junio de 2024, nace este blog. Un blog que pretende hablar sobre
+cualquier cosa que me apetezca relacionada con el hacking. 
+
+Que nadie se espere encontrar contenido profesional y preciso en las entradas de este 
+blog. Todo el contenido será el reflejo de una persona principiante, que aún
+sigue aprendiendo y que no se dedica profesionalmente a esto (puede que algún
+día). 
+
+Exactamente, ¿qué voy a escribir?. Probablemente los famosos *write ups* de
+desafíos CTF (¿¡original verdad!?), conocimientos que vaya adquiriendo por el camino y
+cualquier otra cosa que considere que merece la pena contar.
+
+
+## Continuará...
+
+Poco más que añadir en esta introducción al blog. Espero poder aportar 
+algo a aquellas personas que están comenzando en este mundillo. Nos vemos en la
+siguiente entrada del blog.
+
+<hr>
+
+<i style="font-size: 26px; margin: 0">
+The quieter you become, the more you are able to hear
+</i>
+
---
+++ gatsby-config.js
@@ -0,0 +1,125 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `El blog de n0d0n`,
+    author: {
+      name: `n0d0n`,
+      summary: `desarrollador full stack y aficionado al hacking`,
+    },
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    social: {
+      twitter: `_n0d0n_`,
+    },
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ "content:encoded": node.html }],
+                })
+              })
+            },
+            query: `{
+              allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
+                nodes {
+                  excerpt
+                  html
+                  fields {
+                    slug
+                  }
+                  frontmatter {
+                    title
+                    date
+                  }
+                }
+              }
+            }`,
+            output: "/rss.xml",
+            title: "Gatsby Starter Blog RSS Feed",
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `Gatsby`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/n0d0n-icon.jpeg`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
